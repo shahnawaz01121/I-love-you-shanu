fe7bcaa8b92a692a78a16b577cdee0f1c0ddcbbe
Sumit6728 <monikaraj0954@gmail.com>
2024-12-16 18:59:29 +0530

    Update OWNER.js

--- Priyansh/commands/OWNER.js
+++ Priyansh/commands/OWNER.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Virat") || 
 react.includes("owner")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ fecebook https://www.facebook.com/profile.php?id=100030585286077`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ @Mêlå Bãçhå",
 				attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
