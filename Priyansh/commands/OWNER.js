e70130bc6706c1788ade87f40a247a81f7e5a7d0
Sumit6728 <monikaraj0954@gmail.com>
2025-01-11 17:09:37 +0530

    Update OWNER.js

--- Priyansh/commands/OWNER.js
+++ Priyansh/commands/OWNER.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Virat") || 
 react.includes("owner")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ @Mêlå Bãçhå",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ 𝐎𝐰𝐧𝐞𝐫 ➻  🥀  VIRAT SAINI\n\n● ────────────────── ●\n\n𝐌𝐘 owner Virat\n\n● ────────────────── ●\n\n\n𝐎𝐰𝐧𝐞𝐫 ➻  VIRAT SAINI",
 				attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
