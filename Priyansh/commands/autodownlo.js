000893f35643e0967b8ea089ca0653b999c04967
Sumit6728 <monikaraj0954@gmail.com>
2025-01-17 21:49:59 +0530

    Update autodownlo.js

--- Priyansh/commands/autodownlo.js
+++ Priyansh/commands/autodownlo.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "linkAutoDownload",
     version: "1.3.0",
     hasPermssion: 0,
-    credits: "VIRAT SAINI",
+    credits: "ARIF BABU",
     description:
       "Automatically detects links in messages and downloads the file.",
     commandCategory: "Utilities",
@@ -19,7 +19,7 @@ module.exports = {
     const body = content.toLowerCase();
     const { alldown } = require("nayan-videos-downloader");
     if (body.startsWith("https://")) {
-      api.setMessageReaction("😍", event.messageID, (err) => {}, true);
+      api.setMessageReaction("📿", event.messageID, (err) => {}, true);
       const data = await alldown(content);
       console.log(data);
       const { low, high, title } = data.data;
@@ -36,7 +36,7 @@ module.exports = {
 
       return api.sendMessage(
         {
-          body: `✨❁ ━━ ━[ 😀 ]━ ━━ ❁✨\n\n: ${title}\n\n✨❁ ━━ ━[ 😈 ]━ ━━ ❁✨`,
+          body: ` ཫ ꕥ [ 😀 ]  ꕥ ཀ \n\nᴛɪᴛʟᴇ: ${title}\n\n ཫ ꕥ [ 😈 ] ꕥ ཀ `,
           attachment: fs.createReadStream(__dirname + "/cache/auto.mp4"),
         },
         event.threadID,
