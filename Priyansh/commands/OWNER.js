b0ed00ad766c999758ad4da9a6277aeb76f393d9
Pagal4579 <shreyaahirani402@gmail.com>
2025-06-15 21:22:47 +0530

    Update OWNER.js

--- Priyansh/commands/OWNER.js
+++ Priyansh/commands/OWNER.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("sandeep") || 
 react.includes("owner")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ 𝐎𝐰𝐧𝐞𝐫 ➻  🥀  Sandeep\n\n● ────────────────── ●\n\n𝐌𝐘 owner sandeep\n\n● ────────────────── ●\n\n\n𝐎𝐰𝐧𝐞𝐫 ➻  sandeep",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢 𝗠𝗮𝗱𝗲 𝗕𝘆 ★ 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝 𝐥𝐢𝐧𝐤 👉 https://www.facebook.com/terabaphu727 \n\n● ────────────────── ●\n\n𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢\n\n● ────────────────── ●\n\n\n𝐎𝐰𝐧𝐞𝐫 ➻𝐕𝐢𝐫𝐚𝐭 𝐬𝐚𝐢𝐧𝐢  ",
 				attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
